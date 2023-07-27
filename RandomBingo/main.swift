d1dfa6c53a272700d962d049ac8e7d80c28d94cd
Bucky <bucky5683@gmail.com>
2023-07-27 23:11:58 +0900

    make bingo function

--- RandomBingo/main.swift
+++ RandomBingo/main.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-print("Hello, World!")
+var computerChoice = Int.random(in: 1...100)
 
+var myChoice: Int = 0
+while true {
+    var userInput = readLine() //string 타입으로 받아옴
+    
+    if let input = userInput{ //Optional 벗기기
+        if let number = Int(input){ //Optional 벗기기
+            myChoice = number
+        }
+    }
+    
+    if computerChoice > myChoice{
+        print("up")
+    } else if computerChoice < myChoice{
+        print("down")
+    } else {
+        print("Bingo")
+        break //반복문 종료
+    }
+}
